126ae5a50ddf981988be355bc16a7573193b8c22
Barsa-M <barsamick@gmail.com>
2025-01-02 21:55:15 +0300

    Sidebar and Header updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <>
-      <Sidebar/>
-      <Header/>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 flex flex-col mr-6">
+          <Header />
+        </div>
+      </div>
     </>
   )
 }
